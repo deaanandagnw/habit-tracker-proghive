5fa56b8b22c7c35be332d885332661d3defd672c
Shinta Andrea <shintaandrea96@gmail.com>
2024-10-09 21:18:47 +0700

    hapus header di page js

--- src/actions/auth.register.js
+++ src/actions/auth.register.js
@@ -6,25 +6,25 @@ import { redirect } from "next/navigation";
 import crypto from "crypto"; // For generating a secure session ID
 
 export async function handleRegister(formData) {
-    const username = formData.get("username");
-    const email = formData.get("email");
-    const password = formData.get("password");
+  const username = formData.get("username");
+  const email = formData.get("email");
+  const password = formData.get("password");
 
-    //   AUTHORIZATION CHEKING!!
+  //   AUTHORIZATION CHEKING!!
 
-    const hashedPassword = await bcrypt.hash(password, 12);
-    const newUser = await prisma.user.create({
-        data: {
-            username,
-            email,
-            password: hashedPassword,
-            role: "user",
-            gender: "",
-            country: "",
-            biodata: "",
-        },
-    });
+  const hashedPassword = await bcrypt.hash(password, 12);
+  const newUser = await prisma.user.create({
+    data: {
+      username,
+      email,
+      password: hashedPassword,
 
-    redirect("/login");
-    // console.log(newUser);
+      gender: "",
+      country: "",
+      biodata: "",
+    },
+  });
+
+  redirect("/login");
+  // console.log(newUser);
 }
