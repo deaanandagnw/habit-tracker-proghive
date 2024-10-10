3043ee545a6bb87cedf850728cbc7b3b78fe8af0
deaanandagnw <dea.ananda@onebyone.io>
2024-10-10 17:04:27 +0700

    add page for goal details

---
+++ src/app/goals/[id]/page.jsx
@@ -0,0 +1,20 @@
+"use client";
+
+import React, { useState, useEffect } from "react";
+
+export default function GoalDetailsPage() {
+  return (
+    <div className="container mx-auto p-4">
+      <main className="p-8 w-full">
+        <h2 className="text-dark font-semibold text-3xl">Goal Details 🎯</h2>
+        <p className="text-base text-secondary">
+          Goals goals goals!! Lorem ipsum dolor sit goals.
+        </p>
+        <hr className="w-full bg-[#55A0AC] h-[2px] my-5" />
+        <div className="flex flex-row justify-between items-center gap-8">
+          hah
+        </div>
+      </main>
+    </div>
+  );
+}
