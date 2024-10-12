a6360358d4d31378b7a647f4376d5cf1b753fee9
deaanandagnw <dea.ananda@onebyone.io>
2024-10-12 09:58:56 +0700

    replace class attribute to className

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -24,11 +24,11 @@ export default function profilePage() {
   // const email = formData.get("email");
 
   return (
-    <main class="h-screen w-full grid grid-cols-5  bg-[#DDF0F3] ">
-      <section class=" w-full min-h-full col-span-2 bg-white  pt-10 pb-20 grid grid-rows-2 ">
+    <main className="h-screen w-full grid grid-cols-5  bg-[#DDF0F3] ">
+      <section className=" w-full min-h-full col-span-2 bg-white  pt-10 pb-20 grid grid-rows-2 ">
         <Profile />
       </section>
-      <section class="col-span-3 flex justify-center items-center bg-white pt-20 pb-20">
+      <section className="col-span-3 flex justify-center items-center bg-white pt-20 pb-20">
         <EditProfile />
       </section>
     </main>
--- src/components/Profile/Ediitprofile.jsx
+++ src/components/Profile/Ediitprofile.jsx
@@ -1,48 +1,50 @@
-export  function EditProfile() {
+export function EditProfile() {
   return (
-    <div class="w-[80%] h-[100%] m-auto pt-15 border-slate-500 rounded-3xl p-10  bg-[#DDF0F3]">
-      <div class="w-[600px] space-y-2">
-        <div class="flex flex-row pt-2 pb-5 items-center">
-          <label class=" text-indigo-950 text-2xl">Edit Your Profile ✨</label>
+    <div className="w-[80%] h-[100%] m-auto pt-15 border-slate-500 rounded-3xl p-10  bg-[#DDF0F3]">
+      <div className="w-[600px] space-y-2">
+        <div className="flex flex-row pt-2 pb-5 items-center">
+          <label className=" text-indigo-950 text-2xl">
+            Edit Your Profile ✨
+          </label>
         </div>
-        <div class="flex flex-row">
-          <div class="flex flex-row gap-1 p-2">
+        <div className="flex flex-row">
+          <div className="flex flex-row gap-1 p-2">
             <label>First Name</label>
-            <input class=" border-blue-200 rounded-xl  " />
+            <input className=" border-blue-200 rounded-xl  " />
           </div>
-          <div class="flex flex-row  gap-1 p-2">
+          <div className="flex flex-row  gap-1 p-2">
             <label>Last Name</label>
-            <input class=" border-slate-200 rounded-xl " />
+            <input className=" border-slate-200 rounded-xl " />
           </div>
         </div>
 
-        <div class="grid">
+        <div className="grid">
           <label>Email</label>
-          <input class=" border-slate-200 rounded-xl p-2" />
+          <input className=" border-slate-200 rounded-xl p-2" />
         </div>
 
-        <div class="grid">
+        <div className="grid">
           <label>Password</label>
-          <input class=" border-slate-200 rounded-xl p-2" />
+          <input className=" border-slate-200 rounded-xl p-2" />
         </div>
 
-        <div class="flex flex-row ">
-          <div class="grid w-full">
+        <div className="flex flex-row ">
+          <div className="grid w-full">
             <label>Gender</label>
-            <select class=" border-slate-200 rounded-xl p-2 gap-2">
+            <select className=" border-slate-200 rounded-xl p-2 gap-2">
               <option>Male</option>
               <option>Female</option>
             </select>
           </div>
-          <div class="grid w-full pl-2 ">
+          <div className="grid w-full pl-2 ">
             <label>Birth Date</label>
-            <input class="p-2 rounded-xl" type="date" />
+            <input className="p-2 rounded-xl" type="date" />
           </div>
         </div>
 
-        <div class="grid ">
+        <div className="grid ">
           <label>Country</label>
-          <select class=" border-slate-200 rounded-xl p-2">
+          <select className=" border-slate-200 rounded-xl p-2">
             <option>Indonesia</option>
             <option>Singapore</option>
             <option>malaysia</option>
@@ -50,13 +52,13 @@ export  function EditProfile() {
           </select>
         </div>
 
-        <div class="grid">
+        <div className="grid">
           <label>About Me</label>
-          <textarea class="border-slate-200 rounded-xl p-2" />
+          <textarea className="border-slate-200 rounded-xl p-2" />
         </div>
 
-        <div class="grid">
-          <button class="border-slate-200 rounded-xl p-2 hover:bg-green-400">
+        <div className="grid">
+          <button className="border-slate-200 rounded-xl p-2 hover:bg-green-400">
             Save
           </button>
         </div>
